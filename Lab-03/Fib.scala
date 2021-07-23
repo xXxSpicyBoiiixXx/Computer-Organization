77247a3c2a91e512c95f5138bc05d07c27686461
xXxSpicyBoiiixXx <mali54@hawk.iit.edu>
2021-07-23 13:45:32 -0500

    extra

--- Lab-03/Fib.scala
+++ Lab-03/Fib.scala
@@ -2,7 +2,14 @@ package fib
 
 object Fib extends App {
 
+// F_0 = 0 
+// F_1 = 1 
+// F_n = F_n-1 + F_n-2 
 def fib(arg: Int) : Int = {
+	val F_0 = 0 
+	val F_1 = 1 
+
+// Finish off later 
 	
 }
 
